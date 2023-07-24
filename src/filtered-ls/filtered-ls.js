b5a1382f9a54d4949e23ca45a483505990f30c4a
iamBharatManral <justbharatmanral@gmail.com>
2023-07-24 11:45:17 +0530

    refactor: make filterFile function generic

--- src/filtered-ls/filtered-ls.js
+++ src/filtered-ls/filtered-ls.js
@@ -5,15 +5,15 @@ const listFilesTask = tryCatchAsyncTask(promises.readdir)
 const FILE_PATH = process.argv[2]
 const FILE_EXTENSION = process.argv[3]
 
-const filterFiles = files =>
+const filterFiles = (files, extension) =>
     files
-        .filter(file => file.match(new RegExp(`.${FILE_EXTENSION}`)))
+        .filter(file => file.match(new RegExp(`.${extension}`)))
 
 const printFiles = files => files.forEach(file => console.log(file))
 
 listFilesTask(FILE_PATH, "utf-8")
     .run()
     .listen({
-        onResolved: (files) => printFiles(filterFiles(files)),
+        onResolved: (files) => printFiles(filterFiles(files, FILE_EXTENSION)),
         onRejected: (err) => console.error(err)
     })
